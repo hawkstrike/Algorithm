4343a623af77eb7bfa05a1cb374d12310b78d816
Fedaykin <fremanfedaykin@gmail.com>
2020-01-27 15:03:07 +0900

    Solved Problem at LeetCode by 2020-01-27.

---
+++ src/LeetCode/Algorithms/Find_Numbers_with_Even_Number_of_Digits.java
@@ -0,0 +1,35 @@
+package LeetCode.Algorithms;
+
+// https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+public class Find_Numbers_with_Even_Number_of_Digits {
+	public static void main(String[] args) {
+		int[] nums = {555, 901, 482, 1771};// {12, 345, 2, 6, 7896};
+
+		System.out.println(findNumbers(nums));
+	}
+
+	public static int findNumbers(int[] nums) {
+		int result = 0;
+
+		for(int n : nums) {
+			String temp = String.valueOf(n);
+
+			if(temp.length() % 2 == 0)
+				result++;
+		}
+
+		/*for(int n : nums) {
+			int count = 0;
+
+			while(n > 0) {
+				n /= 10;
+				count++;
+			}
+
+			if(count % 2 == 0)
+				result++;
+		}*/
+
+		return result;
+	}
+}
