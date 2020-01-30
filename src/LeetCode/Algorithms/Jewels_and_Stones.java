6c52b171ed3feaacd96fd213139d5ed913dd3b33
Fedaykin <fremanfedaykin@gmail.com>
2020-01-30 20:16:55 +0900

    Solved Problem at LeetCode on 2020-01-30.

---
+++ src/LeetCode/Algorithms/Jewels_and_Stones.java
@@ -0,0 +1,24 @@
+package LeetCode.Algorithms;
+
+// https://leetcode.com/problems/jewels-and-stones/
+public class Jewels_and_Stones {
+	public static void main(String[] args) {
+		String J = "z"; // "aA";
+		String S = "ZZ"; // "aAAbbbb";
+
+		System.out.println(numJewelsInStones(J, S));
+	}
+
+	public static int numJewelsInStones(String J, String S) {
+		int result = 0;
+
+		for (int i = 0; i < J.length(); i++) {
+			for (int j = 0; j < S.length(); j++) {
+				if (J.charAt(i) == S.charAt(j))
+					result++;
+			}
+		}
+
+		return result;
+	}
+}
