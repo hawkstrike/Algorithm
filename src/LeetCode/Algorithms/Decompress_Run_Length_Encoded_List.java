3635ab2e6790b1be8809a3f25a95d52817af1411
Fedaykin <fremanfedaykin@gmail.com>
2020-02-02 22:51:47 +0900

    Solved Problem at LeetCode on 2020-02-02.

---
+++ src/LeetCode/Algorithms/Decompress_Run_Length_Encoded_List.java
@@ -0,0 +1,38 @@
+package LeetCode.Algorithms;
+
+import java.util.Arrays;
+
+// https://leetcode.com/problems/decompress-run-length-encoded-list/
+public class Decompress_Run_Length_Encoded_List {
+	public static void main(String[] args) {
+		int[] nums = {1, 2, 3, 4};
+
+		int[] result = decompressRLElist(nums);
+
+		for (int n : result) {
+			System.out.print(n + " ");
+		}
+		System.out.println();
+	}
+
+	public static int[] decompressRLElist(int[] nums) {
+		int resultLength = 0;
+		int index = 0;
+
+		for (int i = 0; i < nums.length; i += 2) {
+			resultLength += nums[i];
+		}
+
+		int[] result = new int[resultLength];
+
+		for(int i = 0; i < nums.length; i += 2) {
+			int freq = nums[i];
+			int val = nums[i + 1];
+
+			Arrays.fill(result, index, index + freq, val);
+			index += freq;
+		}
+
+		return result;
+	}
+}
