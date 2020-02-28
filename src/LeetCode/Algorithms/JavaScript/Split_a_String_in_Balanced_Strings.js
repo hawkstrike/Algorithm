6929f087f38f2958f2a26a80a62c6b247c41b948
Fedaykin <fremanfedaykin@gmail.com>
2020-02-28 23:41:40 +0900

    Solved Problem at LeetCode on 2020-02-27.

---
+++ src/LeetCode/Algorithms/JavaScript/Split_a_String_in_Balanced_Strings.js
@@ -0,0 +1,20 @@
+/**
+ * https://leetcode.com/problems/split-a-string-in-balanced-strings/
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function(s) {
+	let result = 0;
+	let balance = 0;
+	
+	for (const c of s) {
+		balance += (c === 'R') ? 1 : -1;
+		result += (balance === 0) ? 1 : 0;
+	}
+	
+	return result;
+};
+
+let s = 'RLRRLLRLRL';
+
+console.log(balancedStringSplit(s));
