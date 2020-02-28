90fee2c906d1a8df8744191e84c6589e4cdc8639
Fedaykin <fremanfedaykin@gmail.com>
2020-02-28 23:42:35 +0900

    Solved Problem at LeetCode on 2020-02-28.

--- src/LeetCode/Algorithms/JavaScript/Split_a_String_in_Balanced_Strings.js
+++ src/LeetCode/Algorithms/JavaScript/Split_a_String_in_Balanced_Strings.js
@@ -1,5 +1,5 @@
 /**
- * https://leetcode.com/problems/split-a-string-in-balanced-strings/
+ * https://leetcode.com/problems/split-a-string-in-balanced-strings
  * @param {string} s
  * @return {number}
  */
