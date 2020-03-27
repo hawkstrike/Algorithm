bc75679b3697b6214dbd4da781c74f74729c65d3
Fedaykin <fremanfedaykin@gmail.com>
2020-03-27 22:21:08 +0900

    Solved Problem at LeetCode on 2020-03-25.

---
+++ src/LeetCode/Algorithms/JavaScript/Number_of_Steps_to_Reduce_a_Number_to_Zero.js
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps  = function(num) {
+	if (num === 0) {
+		return 0;
+	}
+	
+	return 1 + ((num % 2 === 0) ? numberOfSteps(num / 2) : numberOfSteps(num - 1));
+	/*let result = 0;
+	
+	while (num > 0) {
+		num = (num % 2 === 0) ? num / 2 : num - 1;
+		result++;
+	}
+	
+	return result;*/
+};
+
+let num = 14;
+
+console.log(numberOfSteps(num));
