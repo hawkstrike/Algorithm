b5b62ab2857a6fadf6e719c652a1e3d0a926e160
Fedaykin <fremanfedaykin@gmail.com>
2020-07-16 23:33:32 +0900

    Solved Problem at LeetCode on 2020-07-16.

---
+++ src/LeetCode/Algorithms/Java/Pow_x_n.java
@@ -0,0 +1,15 @@
+package LeetCode.Algorithms.Java;
+
+// https://leetcode.com/problems/powx-n/
+public class Pow_x_n {
+	public static void main(String[] args) {
+		double x = 2.00000;
+		int n = 10;
+		
+		System.out.println(myPow(x, n));
+	}
+	
+	public static double myPow(double x, int n) {
+	
+	}
+}
