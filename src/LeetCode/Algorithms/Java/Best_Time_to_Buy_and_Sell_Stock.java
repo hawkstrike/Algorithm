3945a6e87e098b8e9eb5ad69260052ba8d1819ad
Fedaykin <fremanfedaykin@gmail.com>
2020-09-18 20:05:28 +0900

    Solved Problem at LeetCode on 2020-09-18.

---
+++ src/LeetCode/Algorithms/Java/Best_Time_to_Buy_and_Sell_Stock.java
@@ -0,0 +1,25 @@
+package LeetCode.Algorithms.Java;
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+public class Best_Time_to_Buy_and_Sell_Stock {
+	public static void main(String[] args) {
+		int[] prices = {7, 1, 5, 3, 6, 4};
+		
+		System.out.println(maxProfit(prices));
+	}
+	
+	public static int maxProfit(int[] prices) {
+		int maxProfit = 0;
+		int minPrice = Integer.MAX_VALUE;
+		
+		for (int i = 0; i < prices.length; i++) {
+			if (prices[i] < minPrice) {
+				minPrice = prices[i];
+			} else if (prices[i] - minPrice > maxProfit) {
+				maxProfit = prices[i] - minPrice;
+			}
+		}
+		
+		return maxProfit;
+	}
+}
