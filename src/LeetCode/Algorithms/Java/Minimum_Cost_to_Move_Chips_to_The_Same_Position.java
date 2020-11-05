88b506362ec02bed38d0bfbed8b79a758909edb2
fedaykin <fremanfedaykin@gmail.com>
2020-11-05 21:05:14 +0900

    Revert "Solved Problem at LeetCode on 2020-11-05.

---
+++ src/LeetCode/Algorithms/Java/Minimum_Cost_to_Move_Chips_to_The_Same_Position.java
@@ -0,0 +1,25 @@
+package LeetCode.Algorithms.Java;
+
+// https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position/
+public class Minimum_Cost_to_Move_Chips_to_The_Same_Position {
+	public static void main(String[] args) {
+		int[] position = {1, 2, 3};
+		
+		System.out.println(minCostToMoveChips(position));
+	}
+	
+	public static int minCostToMoveChips(int[] position) {
+		int odd = 0;
+		int even = 0;
+		
+		for (int i : position) {
+			if (i % 2 == 0) {
+				even++;
+			} else {
+				odd++;
+			}
+		}
+		
+		return Math.min(odd, even);
+	}
+}
