35bef136cd573d8d0a553bc06e3c257d41b67e2b
fedaykin <fremanfedaykin@gmail.com>
2020-11-17 21:15:46 +0900

    Revert "Solved Problem at LeetCode on 2020-11-17.

---
+++ src/LeetCode/Algorithms/Java/Mirror_Reflection.java
@@ -0,0 +1,34 @@
+package LeetCode.Algorithms.Java;
+
+// https://leetcode.com/problems/mirror-reflection/
+public class Mirror_Reflection {
+	public static void main(String[] args) {
+		int p = 2;
+		int q = 1;
+		
+		System.out.println(mirrorReflection(p, q));
+	}
+	
+	public static int mirrorReflection(int p, int q) {
+		int g = gcd(p, q);
+		
+		p /= g;
+		p %= 2;
+		q /= g;
+		q %= 2;
+		
+		if (p == 1 && q == 1) {
+			return 1;
+		}
+		
+		return (p == 1) ? 0 : 2;
+	}
+	
+	public static int gcd(int a, int b) {
+		if (a == 0) {
+			return b;
+		}
+		
+		return gcd(b % a, a);
+	}
+}
