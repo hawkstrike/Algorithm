a97199fb1445d5403b9e1d0a47773cd40fc4cb91
fedaykin <fremanfedaykin@gmail.com>
2021-03-10 18:57:12 +0900

    Solved Problem at LeetCode on 2021-03-10.

--- src/LeetCode/Algorithms/Java/Integer_to_Roman.java
+++ src/LeetCode/Algorithms/Java/Integer_to_Roman.java
@@ -4,13 +4,14 @@ package LeetCode.Algorithms.Java;
 public class Integer_to_Roman {
 	public static void main(String[] args) {
 		//int num = 3;
-		int num = 58;
+		//int num = 58;
+		int num = 4;
 		
 		System.out.println(intToRoman(num));
 	}
 	
 	public static String intToRoman(int num) {
-		String[] romanArr = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "VI", "I"};
+		String[] romanArr = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 		int[] numberArr = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 		StringBuffer sb = new StringBuffer();
 		
