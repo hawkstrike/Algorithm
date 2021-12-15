6f9ff5dc5778fd8de0cd2d8dbb4726f4f83b0455
fedaykin <fremanfedaykin@gmail.com>
2021-12-15 22:20:16 +0900

    add comment

--- src/Goorm/Kotlin/Level3/Problem_88520.kt
+++ src/Goorm/Kotlin/Level3/Problem_88520.kt
@@ -1,5 +1,6 @@
 package Goorm.Kotlin.Level3
 
+// https://level.goorm.io/exam/88520/%EB%86%80%EC%9D%B4%EA%B3%B5%EC%9B%90/quiz/1
 fun main(args: Array<String>) {
 	val T: Int = readLine()!!.toInt()
 	
